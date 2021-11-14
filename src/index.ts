9eadeff323abe38ae8d4f154384591d8b8f301d4
pkiop <pkiop@naver.com>
2021-11-15 00:12:17 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+function main() {
+  const canvas = document.querySelector('#glCanvas') as HTMLCanvasElement;
+  const gl = canvas.getContext('webgl');
+  if (gl === null) {
+    alert(
+      'Unable to initialize WebGL. Your browser or machine may not support it.'
+    );
+    return;
+  }
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
